d6855825ec26e63707c01f2712f492cd41674cdb
Camie DeCovich <camiedecovich@gmail.com>
2015-09-12 14:19:01 -0800

    Added tests for creating todo items and associated code.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -3,4 +3,28 @@ class TodoItemsController < ApplicationController
   	#find todo_list - get todo_list_id from routes
   	@todo_list = TodoList.find(params[:todo_list_id])
   end
+
+  def new
+  	 @todo_list = TodoList.find(params[:todo_list_id])
+  	@todo_item = @todo_list.todo_items.new
+  end
+
+  def create
+  	@todo_list = TodoList.find(params[:todo_list_id])
+  	@todo_item = todo_items.new(params[todo_item_params])
+  	if @todo_item.save
+  		flash[:success] = "Added todo list item."
+  		redirect_to todo_list_todo_items_path
+  	else
+  		flash[:error] = "There was a problem adding a todo list item."
+  		render action :new
+  	end
+
+  end
+
+
+  private
+  def todo_item_params
+  	params[:todo_item].permit(:content)
+  end
 end
